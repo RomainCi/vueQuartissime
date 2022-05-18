16fde946f02a84dcadd85c190d9db477a41f819d
eric <erichogedez2@gmail.com>
2022-05-18 15:30:39 +0200

    ericdetails3

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,102 +1,124 @@
 <template>
-  <div class="page-map">
-    <div class="map">
-      <l-map style="height: 80vh; width: 80vw" :zoom="zoom" :center="center">
-        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-        <l-marker
-          v-for="comitee in comitees"
-          :key="comitee.id"
-          :lat-lng="[comitee.latitude, comitee.longitude]"
-        >
-          <l-popup
-            ><strong>Nom du comité : </strong> {{ comitee.comiteName }}<br />
-            <strong>Adresse : </strong>{{ comitee.adress }}<br />
-            <strong>Contact : </strong>{{ comitee.phone }}<br /><br />
-            <button
-              type="button"
-              class="btn btn-warning btn-detail"
-              @click="showdetailsComite"
-            >
-              Voir les détails
-            </button>
-          </l-popup>
-        </l-marker>
-      </l-map>
-    </div>
-  </div>
+	<!-- /********************** MAP ***********************/ -->
+	<div v-if="!afficheDetails" id="map" class="page-map">
+		<l-map style="height: 80vh; width: 80vw" :zoom="zoom" :center="center">
+			<l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+			<l-marker
+				v-for="comitee in comitees"
+				:key="comitee.id"
+				:lat-lng="[comitee.latitude, comitee.longitude]"
+			>
+				<l-popup
+					><strong>Nom du comité : </strong> {{ comitee.comiteName }}<br />
+					<strong>Adresse : </strong>{{ comitee.adress }}<br />
+					<strong>Contact : </strong>{{ comitee.phone }}<br /><br />
+					<button
+						type="button"
+						class="btn btn-warning btn-detail"
+						@click="showdetailsComite(comitee.id)"
+					>
+						Voir les détails
+					</button>
+				</l-popup>
+			</l-marker>
+		</l-map>
+	</div>
+
+	<!-- /********************** DÉTAILS COMITE ********************* */ -->
+
+	<div v-else-if="afficheDetails">
+		<p>{{ details.name }}</p>
+		<p>{{ details.adress }}</p>
+		<p>{{ details.phone }}</p>
+	</div>
 </template>
 
+/* ******************* SCRIPT ************************** */
 <script>
 // Inmportation des compsants de leafleats
 import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
+
 const MapComponent = {
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LPopup,
-  },
+	components: {
+		LMap,
+		LTileLayer,
+		LMarker,
+		LPopup,
+	},
 
-  data() {
-    return {
-      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      attribution:
-        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 14,
-      center: [43.7101728, 7.2619532],
-      comitees: [],
-    };
-  },
+	data() {
+		return {
+			url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+			attribution:
+				'&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+			zoom: 14,
+			center: [43.7101728, 7.2619532],
+			comitees: [],
+			details: {},
+			afficheDetails: false,
+		};
+	},
 
-  mounted() {
-    this.getComiteesList();
-  },
+	mounted() {
+		this.getComiteesList();
+	},
 
-  methods: {
-    zoomUpdated(zoom) {
-      this.zoom = zoom;
-      console.log(this.markers);
-    },
+	methods: {
+		zoomUpdated(zoom) {
+			this.zoom = zoom;
+			console.log(this.markers);
+		},
 
-    centerUpdated(center) {
-      this.center = center;
-    },
+		centerUpdated(center) {
+			this.center = center;
+		},
 
-    async getComiteesList() {
-      const promise = await fetch("http://127.0.0.1:8000/api/publics");
-      console.log(promise);
+		/* ********************* RÉCUPÉRATION DES COMITES ************* */
+		async getComiteesList() {
+			const promise = await fetch("http://127.0.0.1:8000/api/publics");
+			console.log(promise);
 
-      let response = await promise.json();
-      console.log(response);
+			let response = await promise.json();
+			console.log(response);
 
-      if (promise.status === 200) {
-        this.comitees = response.comites;
-      }
-    },
+			if (promise.status === 200) {
+				this.comitees = response.comites;
+			}
+		},
 
-    async showdetailsComite() {
-      const promise = await fetch("http://127.0.0.1:8000/api/publics/{id}");
-      console.log("test", promise);
+		/****************** RÉCUPÉRATION DES DÉTAILS D'UN COMITÉ ************/
+		async showdetailsComite(id) {
+			const promise = await fetch("http://127.0.0.1:8000/api/publics/" + id);
+			console.log("test", promise);
 
-      let response = await promise.json();
-      console.log(response);
+			let response = await promise.json();
+			console.log("response", response);
 
-      // if (promise.status === 200) {
-      //   this.comite = response.comites;
-      // }
+			if (promise.status === 200) {
+				this.details = response.detailsComite;
+			}
 
-      this.$router.push("/detailscomite");
-    },
-  },
+			this.afficheDetails = true;
+			/* let map = document.getElementById("map");
+			map.classList.add("hidden");
+
+			let det = document.getElementsById("det");
+			det.classList.remove("hidden"); */
+		},
+	},
 };
 export default MapComponent;
 </script>
 
+/****************** STYLE ************/
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .page-page {
-  display: flex;
-  flex-direction: row;
+	display: flex;
+	flex-direction: row;
 }
+/* .hidden {
+	display: none;
+} */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,50 @@
 <template>
-  <!-- CSS only -->
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-    crossorigin="anonymous"
-  />
-  <div class="home">
-    <div class="div-btn-open-carte">
-      <!-- <button @click="showMap">Voir la carte</button> -->
-      <button
-        type="button"
-        class="btn btn-secondary btn-circle btn-xl"
-        @click="showMap"
-      >
-        Voir la carte
-      </button>
-    </div>
+	<!-- CSS only -->
+	<link
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+		rel="stylesheet"
+		integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+		crossorigin="anonymous"
+	/>
+	<div class="home">
+		<div class="div-btn-open-carte">
+			<button
+				type="button"
+				class="btn btn-secondary btn-circle btn-xl"
+				@click="showMap"
+			>
+				Voir la carte
+			</button>
+		</div>
 
-    <div class="btn-geoc-adresse">
-      <button type="button" class="btn btn-warning">Me géolocaliser</button>
-      <div>
-        <input
-          type="text"
-          class="input-search"
-          placeholder="Saisir une adresse"
-        />
-        <!-- mettre @click la fonction getComites -->
-        <button class="btn-search">Rechercher</button>
-      </div>
-    </div>
+		<div class="btn-geoc-adresse">
+			<button type="button" class="btn btn-warning">Me géolocaliser</button>
+			<div>
+				<input
+					type="text"
+					class="input-search"
+					placeholder="Saisir une adresse"
+				/>
+				<!-- mettre @click la fonction getComites -->
+				<button class="btn-search">Rechercher</button>
+			</div>
+		</div>
 
-    <div v-if="!map" class="img-acceuil">
-      <video
-        type="video/mp4"
-        autoplay
-        :src="carteImg"
-        class="img-carte"
-      ></video>
+		<div id="imgmap" class="img-acceuil">
+			<video
+				type="video/mp4"
+				autoplay
+				:src="carteImg"
+				class="img-carte"
+			></video>
 
-      <img src="../assets/imeuble.png" alt="" class="immeuble" />
-    </div>
+			<img src="../assets/imeuble.png" alt="" class="immeuble" />
+		</div>
 
-    <div v-else-if="map" class="map">
-      <map-component />
-    </div>
-  </div>
+		<div v-if="map" class="map">
+			<map-component />
+		</div>
+	</div>
 </template>
 
 <script>
@@ -54,85 +53,91 @@ import MapComponent from "@/components/MapComponent.vue";
 import carteImg from "@/assets/carte.mp4";
 
 export default {
-  name: "HomeView",
-  components: {
-    MapComponent,
-  },
-  data() {
-    return {
-      carteImg: carteImg,
-      map: false,
-    };
-  },
+	name: "HomeView",
+	components: {
+		MapComponent,
+	},
+	data() {
+		return {
+			carteImg: carteImg,
+			map: false,
+		};
+	},
 
-  methods: {
-    showMap() {
-      return (this.map = !this.map);
-    },
-  },
+	methods: {
+		showMap() {
+			this.map = true;
+			/* afficheDetails = false; */
+			let mapimg = document.getElementById("imgmap");
+			mapimg.classList.add("hidden");
+		},
+	},
 };
 </script>
 
 <style scoped>
 .div-btn-open-carte {
-  display: flex;
-  justify-content: left;
-  margin-left: 10px;
+	display: flex;
+	justify-content: left;
+	margin-left: 10px;
 }
 .img-carte {
-  height: 60vh;
-  margin-top: 52px;
+	height: 60vh;
+	margin-top: 52px;
 }
 .immeuble {
-  text-align: right;
-  height: 65vh;
-  margin-top: -10px;
+	text-align: right;
+	height: 65vh;
+	margin-top: -10px;
 }
 
 @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
-  /* Styles pour cette Media Queries */
+	/* Styles pour cette Media Queries */
 
-  .immeuble {
-    display: none;
-  }
+	.immeuble {
+		display: none;
+	}
 }
 
 .img-acceuil {
-  display: flex;
-  justify-content: right;
-  gap: 20%;
+	display: flex;
+	justify-content: right;
+	gap: 20%;
 }
 
 .btn-geoc-adresse {
-  display: flex;
-  justify-content: center;
-  gap: 25px;
-  margin-bottom: 20px;
+	display: flex;
+	justify-content: center;
+	gap: 25px;
+	margin-bottom: 20px;
 }
 
 .input-search {
-  border: solid 1px black;
-  padding: 6px;
-  border-radius: 3px;
+	border: solid 1px black;
+	padding: 6px;
+	border-radius: 3px;
 }
 .btn-search {
-  border: none;
-  border-radius: 3px;
-  background-color: black;
-  color: white;
-  padding: 7px;
+	border: none;
+	border-radius: 3px;
+	background-color: black;
+	color: white;
+	padding: 7px;
 }
 
 /* button page principale pour afficher la carte */
 
 @media (min-width: 768px) {
-  .button-53 {
-    padding: 0.75rem 3rem;
-    font-size: 1.25rem;
-  }
+	.button-53 {
+		padding: 0.75rem 3rem;
+		font-size: 1.25rem;
+	}
 }
 .map {
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
+}
+.hidden {
+	display: none;
 }
 </style>
