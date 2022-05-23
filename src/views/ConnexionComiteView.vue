a322abe6864c3c44b98ee0162979b2ca9ffa3636
eric <erichogedez2@gmail.com>
2022-05-23 16:46:16 +0200

    Eric

--- src/views/ConnexionComiteView.vue
+++ src/views/ConnexionComiteView.vue
@@ -146,3 +146,5 @@ export default ConnexionComiteView;
 	}
 }
 </style>
+
+/* "Authorization": "Bearer " + localStorage.getItem("token") */
--- src/views/ProfilComiteView.vue
+++ src/views/ProfilComiteView.vue
@@ -1,13 +1,38 @@
 <template>
 	<div>
 		<h1>PROFIL COMITE</h1>
+		<p>{{ comite.comiteName }}</p>
 	</div>
 </template>
 
 <script>
 const ProfilComiteView = {
 	data() {
-		return {};
+		return {
+			comite: {},
+		};
+	},
+	mounted() {
+		this.showComite();
+	},
+	methods: {
+		async showComite() {
+			const promise = await fetch("http://127.0.0.1:8000/api/profilcomite", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: "Bearer " + localStorage.getItem("token"),
+				},
+			});
+			console.log("profil", promise);
+
+			let response = await promise.json();
+			console.log("response", response);
+
+			/* if (promise.status === 200) {
+				this.comite = response;
+			} */
+		},
 	},
 };
 export default ProfilComiteView;
