fb38d1fa912bca5e5eb1a4890eda081d7c557d07
eric <erichogedez2@gmail.com>
2022-05-24 11:15:59 +0200

    Eric1

--- src/views/ProfilComiteView.vue
+++ src/views/ProfilComiteView.vue
@@ -1,41 +1,81 @@
 <template>
-  <div>
-    <h1>PROFIL COMITE</h1>
-    <p>{{ comite.comiteName }}</p>
-  </div>
+	<div>
+		<h1>PROFIL DU COMITÉ</h1>
+		<div class="bloc-details-img">
+			<div class="div-img-personne">
+				<img src="../assets/comite.jpeg" alt="" class="img-personne" />
+			</div>
+			<div class="container-details">
+				<p><strong>Nom du comité : </strong> {{ comite.comiteName }}</p>
+				<p>
+					<i class="fa-solid fa-map-location-dot"></i
+					><strong>Adresse du comité : </strong>{{ comite.adress }}
+				</p>
+				<p><strong>Telephone : </strong>{{ comite.phone }}</p>
+				<p><strong>Email : </strong>{{ comite.email }}</p>
+				<p><strong> Site web : </strong>{{ comite.webSite }}</p>
+				<p><strong>Description : </strong>{{ comite.description }}</p>
+				<p>
+					<strong>Prénom du Président: </strong>{{ comite.firstnamePresident }}
+				</p>
+				<p>
+					<strong>Nom du Président : </strong>{{ comite.lastnamePresident }}
+				</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 const ProfilComiteView = {
-  data() {
-    return {
-      comite: {},
-    };
-  },
-  mounted() {
-    this.showComite();
-  },
-  methods: {
-    async showComite() {
-      const promise = await fetch("http://127.0.0.1:8000/api/profilcomite", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-      });
-      console.log("profil", promise);
+	data() {
+		return {
+			comite: {},
+		};
+	},
+	mounted() {
+		this.showComite();
+	},
+	methods: {
+		async showComite() {
+			const promise = await fetch("http://127.0.0.1:8000/api/profilcomite", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: "Bearer " + localStorage.getItem("token"),
+				},
+			});
+			console.log("profil", promise);
 
-      let response = await promise.json();
-      console.log("response", response);
+			let response = await promise.json();
+			console.log("response", response);
 
-      if (promise.status === 200) {
-        this.comite = response.comite;
-      }
-    },
-  },
+			if (promise.status === 200) {
+				this.comite = response.comite;
+			}
+		},
+	},
 };
 export default ProfilComiteView;
 </script>
 
-<style scoped></style>
+<style scoped>
+.container-details {
+	padding: 25px;
+	border: solid 2px #ffda3e;
+	width: 50%;
+	box-shadow: 30px -16px #ffda3e;
+	border-radius: 15px;
+}
+.img-personne {
+	height: 65vh;
+	margin-left: 10px;
+}
+.bloc-details-img {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	margin-top: 70px;
+	gap: 8%;
+}
+</style>
